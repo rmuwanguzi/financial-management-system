52e4706f476cebd30db9acfadf41084dcd6507f5
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-22 15:28:51 +0300

    add MTOMS/DistributionPercentage/

---
+++ MTOMS/DistributionPercentage/em.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTOMS
+{
+   public partial class em
+    {
+       public enum off_distribution_type
+       {
+           cuc=1,
+           district,
+           lcb,
+           other
+       }
+    }
+}
---
+++ MTOMS/DistributionPercentage/ic/offeringPercentageDistributionC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTOMS.ic
+{
+    public class offeringPercentageDistributionC
+    {
+        public int account_id { get; set; }
+        //
+        private SortedList<em.off_distribution_type, int> d_data { get; set; }
+        public offeringPercentageDistributionC()
+        {
+            d_data = new SortedList<em.off_distribution_type, int>();
+            d_data.Add(em.off_distribution_type.cuc, 0);
+            d_data.Add(em.off_distribution_type.district, 0);
+            d_data.Add(em.off_distribution_type.lcb, 0);
+            d_data.Add(em.off_distribution_type.other, 0);
+        }
+        public int GetPercentage(em.off_distribution_type _type)
+        {
+            return d_data[_type];
+        }
+        public void SetValue(em.off_distribution_type _type, int _val)
+        {
+            d_data[_type] = _val;
+        }
+
+    }
+}
