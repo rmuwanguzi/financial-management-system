bc043af540890fd9b28aaa1a938e471430833fe1
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-22 15:17:43 +0300

    add MTOMS/ic/

---
+++ MTOMS/ic/deleteBaseC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTOMS.ic
+{
+  public class deleteBaseC
+    {
+      public int del_fs_id { get; set; }
+      public DateTime? del_fs_date { get; set; }
+      public string del_fs_time { get; set; }
+      public int del_pc_us_id { get; set; }
+    }
+}
