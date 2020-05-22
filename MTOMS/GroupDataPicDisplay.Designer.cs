69c556ff7f8fd4e853097dc227a02415373d25af
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-22 15:28:38 +0300

    add MTOMS/GroupDataPicDisplay.Designer.cs

---
+++ MTOMS/GroupDataPicDisplay.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace MTOMS
+{
+    partial class GroupDataPicDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.iGrid1 = new TenTec.Windows.iGridLib.iGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.iGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(751, 40);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // iGrid1
+            // 
+            this.iGrid1.Appearance = TenTec.Windows.iGridLib.iGControlPaintAppearance.StyleFlat;
+            this.iGrid1.AutoHeightRowMode = TenTec.Windows.iGridLib.iGAutoHeightRowMode.Cells;
+            this.iGrid1.AutoResizeCols = true;
+            this.iGrid1.BorderStyle = TenTec.Windows.iGridLib.iGBorderStyle.None;
+            this.iGrid1.CellCtrlBackColor = System.Drawing.Color.Gainsboro;
+            this.iGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.iGrid1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.iGrid1.Header.Appearance = TenTec.Windows.iGridLib.iGControlPaintAppearance.StyleFlat;
+            this.iGrid1.Header.Height = 19;
+            this.iGrid1.Header.Visible = false;
+            this.iGrid1.Location = new System.Drawing.Point(0, 40);
+            this.iGrid1.Name = "iGrid1";
+            this.iGrid1.RowHeader.Visible = true;
+            this.iGrid1.RowHeader.Width = 3;
+            this.iGrid1.RowResizeMode = TenTec.Windows.iGridLib.iGRowResizeMode.NotAllowed;
+            this.iGrid1.RowSelectionInCellMode = TenTec.Windows.iGridLib.iGRowSelectionInCellModeTypes.SingleRow;
+            this.iGrid1.SelCellsBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.iGrid1.SelCellsForeColor = System.Drawing.Color.DarkBlue;
+            this.iGrid1.SilentValidation = true;
+            this.iGrid1.Size = new System.Drawing.Size(751, 483);
+            this.iGrid1.TabIndex = 14;
+            this.iGrid1.TreeCol = null;
+            this.iGrid1.TreeLines.Color = System.Drawing.SystemColors.WindowText;
+            // 
+            // GroupDataPicDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 523);
+            this.Controls.Add(this.iGrid1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GroupDataPicDisplay";
+            this.Text = "GroupData Picture Display";
+            this.Load += new System.EventHandler(this.GroupDataPicDisplay_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.iGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private TenTec.Windows.iGridLib.iGrid iGrid1;
+    }
+}
