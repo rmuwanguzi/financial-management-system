177178582c2fcbb8d0501d9e24816c2d8034e3fe
Ronald Muwanguzi <ronald.muwanguzi.256@gmail.com>
2020-05-22 15:17:59 +0300

    add MTOMS/form_data.cs

---
+++ MTOMS/form_data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTOMS
+{
+  public class form_data 
+    {
+        public IntPtr ParentHandle
+        {
+            get;
+            set;
+        }
+        public object edit_object
+        {
+            get;
+            set;
+        }
+        public object Tag { get; set; }
+          
+    }
+}
