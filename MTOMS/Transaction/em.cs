b4c1b6e878d95ce37359f31ab9ffd28925723c65
David Mukiibi <davidmukiibi@Davids-MacBook-Pro-2.local>
2020-05-22 15:14:36 +0300

    add MTOMS/Transactions

---
+++ MTOMS/Transaction/em.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTOMS
+{
+   public partial class em
+    {
+       public enum acc_trans_typeS
+       {
+           offertory_payment = 0,
+           opening_balance=1,
+           bank_deposit=2,
+           bank_withdraw=3,
+           sabbath_loss=4,
+           sabbath_gain=5,
+           expense=6,
+           expense_payment=7,
+           accounts_adjustment=8
+       }
+    }
+}
---
+++ MTOMS/Transaction/ic/transactionC.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MTOMS.ic
+{
+    public class transactionC
+    {
+        public long trans_id { get; set; }
+        public DateTime? trans_date { get; set; }
+      
+    }
+}
